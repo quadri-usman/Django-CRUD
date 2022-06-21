2649f1a03dbdb4c723fccbf1bd843484155a8e5c
quadri-usman <105228467+quadri-usman@users.noreply.github.com>
2022-06-21 08:08:22 -0700

    Update views.py

--- I4G002905Z7C/blog/views.py
+++ I4G002905Z7C/blog/views.py
@@ -36,6 +36,43 @@ class PostUpdateView(UpdateView):
     model = Post
     success_url = reverse_lazy('post-list')
 
+    
+from .models import GeeksModel
+from .forms import GeeksForm
+ 
+# after updating it will redirect to detail_View
+def detail_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+  
+    # add the dictionary during initialization
+    context["data"] = GeeksModel.objects.get(id = id)
+          
+    return render(request, "detail_view.html", context)
+ 
+# update view for details
+def update_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # fetch the object related to passed id
+    obj = get_object_or_404(GeeksModel, id = id)
+ 
+    # pass the object as instance in form
+    form = GeeksForm(request.POST or None, instance = obj)
+ 
+    # save the data from the form and
+    # redirect to detail_view
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/"+id)
+ 
+    # add form dictionary to context
+    context["form"] = form
+ 
+    return render(request, "update_view.html", context)
 # PostDeleteView
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
@@ -44,3 +81,22 @@ from blog.models import Post
 class PostDeleteView(DeleteView):
     model = Post
     success_url = reverse_lazy('post-list')
+
+    
+def delete_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # fetch the object related to passed id
+    obj = get_object_or_404(GeeksModel, id = id)
+ 
+ 
+    if request.method =="POST":
+        # delete object
+        obj.delete()
+        # after deleting redirect to
+        # home page
+        return HttpResponseRedirect("/")
+ 
+    return render(request, "delete_view.html", context)
